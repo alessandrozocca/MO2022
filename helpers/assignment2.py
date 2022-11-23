8e1a14a6d3ec3d41ee3909f0238773fb9436be85
l-lan <l.lan@vu.nl>
2022-11-23 11:44:45 +0100

    Add assignment 2 data files to repo

--- helpers/assignment2.py
+++ helpers/assignment2.py
@@ -1,6 +1,7 @@
 from itertools import product
 
 import numpy as np
+import pandas as pd
 
 
 def sample_wind_vector(seed):
@@ -54,3 +55,51 @@ def generate_instance(x, y, **kwargs):
     }
 
     return data
+
+
+def read_elastic_net_data():
+    """
+    Returns a features matrix X and the target vector y.
+    """
+    wind_speed = pd.read_csv("data/elastic_net_wind_speed.csv").dropna()
+    X = wind_speed[
+        ["IND", "RAIN", "IND.1", "T.MAX", "IND.2", "T.MIN", "T.MIN.G"]
+    ].values
+    y = wind_speed["WIND"].values
+    return X, y
+
+
+def generate_wind_farm_data():
+    """
+    Computes all data needed for the wind farm part of the assignment.
+    """
+    grid_x_size = 10
+    grid_y_size = 10
+    n_locations = grid_x_size * grid_y_size
+    instance = generate_instance(grid_x_size, grid_y_size)
+
+    samples = 50
+    wind_vectors = np.zeros((samples, 2))
+    interference_matrices = np.zeros((samples, n_locations, n_locations))
+    for i, seed in enumerate(range(samples)):
+        wind_vectors[i] = sample_wind_vector(seed)
+        interference_matrices[i] = compute_interferences(
+            instance["coords"], wind_vectors[i]
+        )
+
+    wind_average = np.average(wind_vectors, axis=0)
+    interference_average = compute_interferences(instance["coords"], wind_average)
+
+    return instance, wind_average, interference_average, interference_matrices
+
+
+def read_economic_dispatch_data():
+    # Download the data
+    nodes_df = pd.read_csv("data/economic_dispatch_nodes.csv", index_col=0)
+    discrete_wind_df = pd.read_csv("data/economic_dispatch_wind_production.csv").T
+
+    # Read data
+    nodes = nodes_df.set_index("node_id").T.to_dict()
+    discrete_wind = list(discrete_wind_df.to_dict().values())
+
+    return nodes, discrete_wind
