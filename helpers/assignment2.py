045bc8b3c13636411b38e15900ff63f13fb99982
l-lan <l.lan@vu.nl>
2022-11-23 16:25:10 +0100

    Update data generation

--- helpers/assignment2.py
+++ helpers/assignment2.py
@@ -1,4 +1,3 @@
-import importlib.resources as pkg_resource
 from itertools import product
 
 import numpy as np
@@ -11,9 +10,11 @@ def sample_wind_vector(seed):
     """
     rng = np.random.default_rng(seed)
 
-    angle = np.pi * np.random.uniform(0, 0.5)
+    target_angle = 0.4
+    width = 0.65
+    angle = np.pi * rng.uniform(target_angle - width, target_angle + width)
     wind_direction = np.array([np.cos(angle), np.sin(angle)])
-    wind_speed = rng.integers(5, 25)
+    wind_speed = 33 * rng.uniform() * max(np.cos(angle - target_angle), 0)
 
     return wind_direction * wind_speed
 
@@ -31,7 +32,7 @@ def compute_interferences(coords, wind_vector):
             if np.any(coord_i != coord_j):
                 d_ij = coord_i - coord_j
 
-                projection = np.dot(d_ij, wind_vector)
+                projection = 20 * np.dot(d_ij, wind_vector)
                 strength = np.linalg.norm(projection) / np.linalg.norm(d_ij) ** 2
 
                 if projection > 0:
@@ -50,9 +51,9 @@ def generate_instance(x, y, **kwargs):
         "n_sites": x * y,
         "coords": coords,
         "production": 150,
-        "min_distance": kwargs.get("min_distance", 2),
-        "min_turbines": kwargs.get("min_turbines", 5),
-        "max_turbines": 30,
+        "min_distance": kwargs.get("min_distance", 1.9),
+        "min_turbines": kwargs.get("min_turbines", 3),
+        "max_turbines": 6,
     }
 
     return data
@@ -76,14 +77,16 @@ def generate_wind_farm_data():
     """
     Computes all data needed for the wind farm part of the assignment.
     """
-    grid_x_size = 10
-    grid_y_size = 10
-    n_locations = grid_x_size * grid_y_size
-    instance = generate_instance(grid_x_size, grid_y_size)
+    grid_x = 10
+    grid_y = 10
+
+    n_locations = grid_x * grid_y
+    instance = generate_instance(grid_x, grid_y)
 
     samples = 50
     wind_vectors = np.zeros((samples, 2))
     interference_matrices = np.zeros((samples, n_locations, n_locations))
+
     for i, seed in enumerate(range(samples)):
         wind_vectors[i] = sample_wind_vector(seed)
         interference_matrices[i] = compute_interferences(
@@ -100,13 +103,15 @@ def read_economic_dispatch_data():
     nodes_df = pd.read_csv(
         "https://gist.githubusercontent.com/leonlan/8145e4477dabe97705c60aa4d55363f5/raw/6ab2d382a0634125aa25f469faa1d7a03afb8596/nodes.csv",
         index_col=0,
-    )
-    discrete_wind_df = pd.read_csv(
-        "https://gist.githubusercontent.com/leonlan/8145e4477dabe97705c60aa4d55363f5/raw/7816951386b4cdd2b624b0c4a34a6c8b66bc1dc8/discrete_wind.csv"
+    )[["node_id", "d", "p_min", "p_max", "c_var"]]
+
+    wind_production_samples_df = pd.read_csv(
+        "https://gist.githubusercontent.com/leonlan/8145e4477dabe97705c60aa4d55363f5/raw/7816951386b4cdd2b624b0c4a34a6c8b66bc1dc8/wind_production_samples.csv"
     ).T
 
     # Read data
     nodes = nodes_df.set_index("node_id").T.to_dict()
-    discrete_wind = list(discrete_wind_df.to_dict().values())
+    wind_production_samples = list(wind_production_samples_df.to_dict().values())
+    wind_production_samples = [sum(d.values()) for d in wind_production_samples]
 
-    return nodes, discrete_wind
+    return nodes, wind_production_samples
