6086cf444ce1ce70cdced68b87e46bf01f4d28c6
l-lan <l.lan@vu.nl>
2022-11-25 16:52:43 +0100

    Add labels to plot_wind_farm function

--- helpers/assignment2.py
+++ helpers/assignment2.py
@@ -121,15 +121,21 @@ def plot_wind_farm(instance, solution, **kwargs):
 
     _, ax = plt.subplots(figsize=[10, 10])
 
-    ax.scatter(coords[:, 0], coords[:, 1], s=10, label="site")
+    ax.scatter(coords[:, 0], coords[:, 1], s=100, label="site")
+
+    for idx in range(len(coords)):
+        margin = 0.05
+        ax.annotate(idx, (coords[idx, 0] + margin, coords[idx, 1] + margin))
 
     turbine_coords = coords[solution]
-    ax.scatter(*turbine_coords.T, s=30, label="turbine")
+    ax.scatter(*turbine_coords.T, s=100, label="turbine")
 
     for (x, y) in turbine_coords:
         cir = plt.Circle((x, y), min_distance / 2, color="r", fill=False)
         ax.add_patch(cir)
 
-    ax.set_aspect("equal", adjustable="datalim")
+    ax.set_xlim(coords[:, 0].min() - 1, coords[:, 0].max() + 1)
+    ax.set_ylim(coords[:, 1].min() - 1, coords[:, 1].max() + 1)
+
     ax.set_title(f"Wind farm layout\nSolution {solution}")
     ax.legend()
