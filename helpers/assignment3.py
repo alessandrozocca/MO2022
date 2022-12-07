409d95690d1884f3e647052288bf7433fbdcbb7f
Jasper van Doorn <jasper.vandoorn@hotmail.com>
2022-12-07 22:12:43 +0100

    Refactor function names

--- helpers/assignment3.py
+++ helpers/assignment3.py
@@ -28,7 +28,7 @@ def get_distances_to(graph, node):
     return nx.single_source_dijkstra_path_length(graph, node, weight="length")
  
  
-def get_crowdedness_coefficents(graph):
+def get_crowdedness(graph):
     c = 5395065019
     delta = get_distances_to(graph, c)
     return {k: 2*math.exp(-v/250) if k != c else 2 for k, v in delta.items()}
