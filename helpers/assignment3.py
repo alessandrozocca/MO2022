d76e426a302cac9b89837a8f520e6244d0f45475
Jasper van Doorn <jasper.vandoorn@hotmail.com>
2022-12-07 23:03:50 +0100

    Add get route

--- helpers/assignment3.py
+++ helpers/assignment3.py
@@ -23,7 +23,14 @@ def make_instance(graph):
 
     return nodes, edges, pred, succ
     
- 
+
+def get_route(model):
+    route = [model.s()]
+    while route[-1] != model.t():
+        route.append(max(model.nodes, key=lambda i: model.x[route[-1], i]() if (route[-1], i) in model.edges else -1))
+    return route
+
+
 def get_distances_to(graph, node):
     return nx.single_source_dijkstra_path_length(graph, node, weight="length")
  
