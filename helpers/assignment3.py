f06e1a88edf025c1c6fe3fb917752784a1bf73a2
Jasper van Doorn <jasper.vandoorn@hotmail.com>
2022-12-07 20:29:46 +0100

    Add lists of neighbors

--- helpers/assignment3.py
+++ helpers/assignment3.py
@@ -19,8 +19,11 @@ def make_instance(graph):
         Returns a list of nodes and a dictionary of edges and their distances. """
     nodes = list(graph.nodes)
     edges = nx.get_edge_attributes(nx.DiGraph(graph), "length")
+    
+    pred = {i: [j for j in nodes if (j, i) in edges] for i in nodes}
+    succ = {i: [j for j in nodes if (i, j) in edges] for i in nodes}
 
-    return nodes, edges
+    return nodes, edges, pred, succ
     
     
 def plot_network(graph, *routes):
